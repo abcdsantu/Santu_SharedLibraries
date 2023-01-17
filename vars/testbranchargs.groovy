0938d11e22f4622831c2a03e41d4a667c805de2b
SNP <119073960+abcdsantu@users.noreply.github.com>
2023-01-17 09:50:56 +0000

    Update testbranchargs.groovy

--- vars/testbranchargs.groovy
+++ vars/testbranchargs.groovy
@@ -43,19 +43,10 @@ def call(String repoUrl) {
 					}
 				}
 			}
-			stage ("UploadToNexus") {
-				steps {
-					script {
-						UploadtoNexus()
-					}
-				}
-				
-			}
+			
 						
 		}
 	
 	}
-	def UploadtoNexus() {
-		nexusArtifactUploader artifacts: [[artifactId: 'myweb', classifier: '', file: 'target/myweb-1.0.0-SNAPSHOT.war', type: 'war']], credentialsId: 'nexus3', groupId: 'in.javahome', nexusUrl: '192.168.145.131:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'Jenkinstest', version: '1.0.0-SNAPSHOT'
-	}
+	
 }
