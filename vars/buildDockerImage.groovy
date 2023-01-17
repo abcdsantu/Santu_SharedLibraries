c6317639c2806863e6aa2959328ed96607bf2886
SNP <119073960+abcdsantu@users.noreply.github.com>
2023-01-17 11:50:27 +0000

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -4,7 +4,8 @@ def call(String imageName){
     echo "Building Docker image..."
     withCredentials([usernamePassword(credentialsId: "dockerhubID", usernameVariable: "username", passwordVariable: "password")]){
         sh "docker build -t $imageName ."
+        sh "docker tag $imageName $imageName+':V$BUILD_NUMBER'"
         sh "echo $password | docker login -u $username --password-stdin"
-        sh "docker push $imageName+":V$BUILD_NUMBER""
+        sh "docker push $imageName+':V$BUILD_NUMBER'"
     }
 }
