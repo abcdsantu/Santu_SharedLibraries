3aa468b99a6ef35816f3a71954e05b6ffe486508
SNP <119073960+abcdsantu@users.noreply.github.com>
2023-01-17 07:45:28 +0000

    Update jenkinsForJava.groovy

--- vars/jenkinsForJava.groovy
+++ vars/jenkinsForJava.groovy
@@ -7,29 +7,14 @@ def call(String repoUrl) {
            
        }
        stages {
-           stage("Tools initialization") {
-               steps {
-                   sh "mvn --version"
-                   sh "java -version"
-               }
-           }
+ 
            stage("Checkout Code") {
                steps {
                    git branch: 'main',
                        url: "${repoUrl}"
                }
            }
-           stage("Cleaning workspace") {
-               steps {
-                   sh "mvn clean"
-               }
-           }
 
-           stage("Packing Application") {
-               steps {
-                   sh "mvn package -DskipTests"
-               }
-           }
        }
    }
 }
