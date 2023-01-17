06f5e09101c58239b9324211e24ba08979592d84
SNP <119073960+abcdsantu@users.noreply.github.com>
2023-01-17 08:47:48 +0000

    Update sonarQuality.groovy

--- vars/sonarQuality.groovy
+++ vars/sonarQuality.groovy
@@ -10,3 +10,27 @@ def call() {
         }
     }
 }
+
+def call() {
+  pipeline {
+       agent any
+       stages {
+
+		    stage ("Sonar Quality Gate status Checks") {
+              steps {
+                 script {
+                    timeout(time: 1, unit: "HOURS") {
+                        def qg = waitForQualityGate()
+                        if (qg.status != "OK") {
+                            error "Aborting the pipeline due to Quality Gate failed: ${qg.status}"
+                        } else {
+							 echo "Quality gate passed with result: ${qg.status}"
+							 }
+                    }
+                }
+            }
+        }
+       }
+  }
+    
+}
